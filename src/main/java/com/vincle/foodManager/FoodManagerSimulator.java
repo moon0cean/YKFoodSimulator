a20b48761603dd40aaad26557684abfd4a93c1e4
Jonatan Soto <jonatan.soto@ilscipio.com>
2024-03-01 09:45:51 +0100

    using env var for food-manager api + delaying app startup (20s) in order to wait for other services to fully boot (specially when running in docker-compose)

--- src/main/java/com/vincle/foodManager/FoodManagerSimulator.java
+++ src/main/java/com/vincle/foodManager/FoodManagerSimulator.java
@@ -9,6 +9,7 @@ import com.vincle.foodManager.model.Uom;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -20,6 +21,7 @@ import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 @SpringBootApplication
@@ -30,11 +32,20 @@ public class FoodManagerSimulator implements ApplicationRunner {
     @Autowired
     DefaultSchedulerConfig defaultSchedulerConfig;
 
+    @Value("${food-manager.host.url}")
+    String foodManagerHostUrl;
+
     private DefaultApi api;
 
     private static final Logger logger = LoggerFactory.getLogger(FoodManagerSimulator.class);
 
     public static void main(String[] args) {
+        // FIXME: nasty workaround as docker-compose doesn't honor depend_on and starts this app first
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         SpringApplication.run(FoodManagerSimulator.class, args);
     }
 
@@ -55,8 +66,13 @@ public class FoodManagerSimulator implements ApplicationRunner {
 
     @Bean
     public DefaultApi getApi() {
-        if (api == null)
-            return new DefaultApi();
+        logger.info("foodManagerHostUrl: " + foodManagerHostUrl);
+        if (api == null) {
+            DefaultApi api = new DefaultApi();
+            api.getApiClient().setServers(List.of(new ServerConfiguration((foodManagerHostUrl != null) ? foodManagerHostUrl : "http://localhost:8080",
+                    null, new HashMap<>())));
+            return api;
+        }
         return api;
     }
 
